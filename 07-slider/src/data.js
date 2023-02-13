3757d92299d97d2ddcebe91079c561d453aac9eb
Bhavan Sai <91448593+BhavanSai12@users.noreply.github.com>
2023-02-13 17:28:49 +0530

    React Projects

---
+++ 07-slider/src/data.js
@@ -0,0 +1,41 @@
+const people = [
+    {
+      id: 1,
+      image:
+        'https://res.cloudinary.com/bbs123/image/upload/c_thumb,w_200,g_face/v1676283571/Photos/eebb3a3a0d234620c7083b58e0db9bc8_qhn9k8.jpg',
+      name: 'Sai Pallavi',
+      title: 'office manager',
+      quote:
+        'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+    },
+    {
+      id: 2,
+      image:
+        'https://res.cloudinary.com/bbs123/image/upload/c_thumb,w_200,g_face/v1676270124/Photos/a4afa7ad4c9afb3256ad0c415a8ea972_gol189.jpg',
+      name: 'Bhanumathi',
+      title: 'Team lead',
+      quote:
+        'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+    },
+    {
+      id: 3,
+      image:
+        'https://res.cloudinary.com/bbs123/image/upload/v1676270129/Photos/aaaad199f3d3079dddde75788e61fe89_d137od.jpg',
+      name: 'Mounika',
+      title: 'product designer',
+      quote:
+        'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+    },
+    {
+      id: 4,
+      image:
+        'https://res.cloudinary.com/bbs123/image/upload/c_thumb,w_200,g_face/v1676270168/Photos/4d535db152918c16b6d80c48080c5cbb_jhsi6i.jpg',
+      name: 'vaishali',
+      title: 'the boss',
+      quote:
+        'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+    },
+  ];
+  
+  export default people;
+  
